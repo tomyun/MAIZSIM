a9c8ba71df2374303236a6dc7379a3d0b0ae1292
Kyungdahm Yun <tomyun@gmail.com>
2015-01-19 21:18:08 -0800

    Create soil module using F2Py

---
+++ driver/f2py/make.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+find ../soil/ | grep -e ".*\.\(for\|FOR\)" | sed "/2DMAIZSIM.for/ d" | sed "/solupt.for/ d" | xargs f2py2.7 -c -m soil --f77flags="--std=legacy -fno-align-commons"
+ln -sf f2py/soil.so ../
