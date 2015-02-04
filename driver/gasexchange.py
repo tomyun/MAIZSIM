69f3130f476a25c52de1a4cca45f14fd2a145d20
Kyungdahm Yun <tomyun@gmail.com>
2015-02-04 11:58:33 -0800

    Clean up legacy _photosynthesis()

--- driver/gasexchange.py
+++ driver/gasexchange.py
@@ -15,7 +15,6 @@
 import numpy as np
 import scipy.optimize
 
-R = 8.314
 MAXITER = 200
 #FIXME are they parameters?
 EPS = 0.97
@@ -49,6 +48,7 @@ class Atmosphere:
     def vapor_pressure_deficit(cls, t, rh):
         es = cls.saturation_vapor_pressure(t)
         return es * (1 - rh)
+
 
 class Stomata:
     def __init__(self):
@@ -169,6 +169,14 @@ class Photosynthesis:
         # Values in Kim (2006) are for 31C, and the values here are normalized for 25C. SK
         self.rd25 = 2.
         self.ear = 39800.
+
+        #FIXME are they even used?
+        #self.beta_aba = 1.48e2 # Tardieu-Davies beta, Dewar (2002) Need the references !?
+        #self.delta = -1.0
+        #self.alpha_aba = 1.0e-4
+        #self.lambda_r = 4.0e-12 # Dewar's email
+        #self.lambda_l = 1.0e-12
+        #self.k_max = 6.67e-3 # max. xylem conductance (mol m-2 s-1 MPa-1) from root to leaf, Dewar (2002)
 
     def _light(self, pfd):
         #FIXME make scatt global parameter?
@@ -300,16 +308,10 @@ class Photosynthesis:
 
         #FIXME avoid passing self.stomata object to optimizer
         # iteration to obtain Cm from Ci and A, could be re-written using more efficient method like newton-raphson method
-        #FIXME would it be okay to access self.a_net here?
-        res = scipy.optimize.minimize(cost, [9999999], options={'disp': True})
+        res = scipy.optimize.minimize(cost, [0], options={'disp': True})
         a_net = res.x[0]
         self.stomata.update_stomata(pressure, co2, a_net, rh, tleaf)
         return a_net
-
-        cm = co2_mesophyll(self.a_net, press, co2, self.stomata)
-        self.ci = cm
-
-        self.a_gross = max(0, self.a_net + rd) # gets negative when PFD = 0, Rd needs to be examined, 10/25/04, SK
 
 
 class GasExchange:
@@ -338,36 +340,8 @@ class GasExchange:
         self.leafp = leafp
         self.leafp_effect = 1 # At first assume there is not drought stress, so assign 1 to leafpEffect. Yang 8/20/06
 
-        self._setup_parms()
-
         # override GasEx() function so as to pass leaf water potential
         self._gasex_psil(leafp, et_supply)
-
-    def _setup_parms(self):
-        #FIXME move most of them into Photosynthesis class
-        self.eavp = 75100
-        self.eavc = 55900 # Sage (2002) JXB
-        self.eaj = 32800
-        self.hj = 220000
-        self.sj = 702.6
-
-        # Kim et al. (2007), Kim et al. (2006)
-        # In von Cammerer (2000), Vpm25=120, Vcm25=60,Jm25=400
-        # In Soo et al.(2006), under elevated C5O2, Vpm25=91.9, Vcm25=71.6, Jm25=354.2 YY
-        self.vpm25 = 70.0
-        self.vcm25 = 50.0
-        self.jm25 = 300.0
-
-        # Values in Kim (2006) are for 31C, and the values here are normalized for 25C. SK
-        self.rd25 = 2.0
-        self.ear = 39800
-
-        self.beta_aba = 1.48e2 # Tardieu-Davies beta, Dewar (2002) Need the references !?
-        self.delta = -1.0
-        self.alpha_aba = 1.0e-4
-        self.lambda_r = 4.0e-12 # Dewar's email
-        self.lambda_l = 1.0e-12
-        self.k_max = 6.67e-3 # max. xylem conductance (mol m-2 s-1 MPa-1) from root to leaf, Dewar (2002)
 
     def _gasex_psil(self, leafp, et_supply):
         ca = self.co2
@@ -394,10 +368,9 @@ class GasExchange:
         while abs(tleaf_old - self.tleaf) > 0.01 and i < MAXITER:
             tleaf_old = self.tleaf
             #FIXME minimize side-effects in _photosynthesis()
-            #self._photosynthesis(leafp, self.tleaf)
-
             self.a_net = self.photosynthesis.photosynthesize(self.pfd, self.press, self.co2, self.rh, leafp, self.tleaf)
 
+            #FIXME can this be moved out?
             cm = self.photosynthesis._co2_mesophyll(self.a_net, self.press, self.co2, self.stomata)
             self.ci = cm
 
@@ -405,142 +378,10 @@ class GasExchange:
             self.a_gross = max(0, self.a_net + rd) # gets negative when PFD = 0, Rd needs to be examined, 10/25/04, SK
 
             self.tleaf = self._energybalance(et_supply)
-
-            #self._evapotranspiration(self.tair, self.tleaf)
             i += 1
             #FIXME remove
             self.iter2 = i
         self._evapotranspiration(self.tair, self.tleaf)
-
-    # Incident PFD, Air temp in C, CO2 in ppm, RH in percent
-    def _photosynthesis(self, pressure, tleaf):
-        f = 0.15 #spectral correction
-        o = 210 # gas units are mbar
-        theta = 0.5
-        #FIXME make scatt global parameter?
-        scatt = 0.15 # leaf reflectance + transmittance
-        kc25 = 650 # Michaelis constant of rubisco for CO2 of C4 plants (2.5 times that of tobacco), ubar, Von Caemmerer 2000
-        ko25 = 450 # Michaelis constant of rubisco for O2 (2.5 times C3), mbar
-        kp25 = 80 # Michaelis constant for PEP caboxylase for CO2
-        eac = 59400
-        eao = 36000 # activation energy values
-        vpr = 80 # PEP regeneration limited Vp, value adopted from vC book
-        gbs = 0.003 # bundle sheath conductance to CO2, mol m-2 s-1
-        x = 0.4 # Partitioning factor of J, yield maximal J at this value
-        alpha = 0.0001 # fraction of PSII activity in the bundle sheath cell, very low for NADP-ME types
-        gi = 1.0 # conductance to CO2 from intercelluar to mesophyle, mol m-2 s-1, assumed
-        beta = 0.99 # smoothing factor
-        gamma1 = 0.193 # half the reciprocal of rubisco specificity, to account for O2 dependence of CO2 comp point, note that this become the same as that in C3 model when multiplied by [O2]
-
-        #FIXME is it necessary?
-        # Reset values changed for N status
-        self._setup_parms()
-
-        # variables
-        pfd = self.pfd
-        #tleaf = self.tleaf
-        press = self.press
-        co2 = self.co2
-
-        # params
-        rd25 = self.rd25
-        ear = self.ear
-        vcm25 = self.vcm25
-        jm25 = self.jm25
-        vpm25 = self.vpm25
-        eavp = self.eavp
-        eavc = self.eavc
-        eaj = self.eaj
-        sj = self.sj
-        hj = self.hj
-
-        #double Kp, Kc, Ko, Km, Ia, I2, Tk, Vpmax, Jmax, Vcmax, Om, Rm, J, Ac1, Ac2, Ac, Aj1,
-        #       Aj2, Aj, Vp1, Vp2, Vp, P,  Ca, Cm, Cm_last, Cm_next,
-        #       Os, GammaStar, Gamma;
-
-        # Light response function parameters
-        ia = pfd * (1 - scatt) # absorbed irradiance
-        i2 = ia * (1 - f) / 2. # useful light absorbed by PSII
-
-        # other input parameters and constants
-        tk = tleaf + 273.0
-        p = press / 100.
-
-        #FIXME name it properly
-        tleaf_ratio = (tleaf - 25) / (298 * R * (tleaf + 273))
-
-        ca = co2 * p # conversion to partial pressure
-        om = o # mesophyle O2 partial pressure
-        kp = kp25 # T dependence yet to be determined
-        kc = kc25 * np.exp(eac * tleaf_ratio)
-        ko = ko25 * np.exp(eao * tleaf_ratio)
-        km = kc * (1 + om / ko) # effective M-M constant for Kc in the presence of O2
-        rd = rd25 * np.exp(ear * tleaf_ratio)
-
-        critical_nitrogen = max(0.25, self.leaf_n_content)
-        cnr = 2 / (1 + np.exp(-2.9 * (critical_nitrogen - 0.25))) - 1
-        vcm25 *= cnr
-        jm25  *= cnr
-        vpm25 *= cnr
-        # in Sinclair and Horie, 1989 Crop sciences, it is 4 and 0.2
-        # In J Vos. et al. Field Crop study, 2005, it is 2.9 and 0.25
-        # In Lindquist, weed science, 2001, it is 3.689 and 0.5
-
-        vpmax = vpm25 * np.exp(eavp * tleaf_ratio)
-        vcmax = vcm25 * np.exp(eavc * tleaf_ratio)
-        jmax  = jm25  * np.exp(((tk - 298) * eaj) / (R * tk * 298)) \
-                * (1 + np.exp((sj * 298 - hj) / (R * 298))) \
-                / (1 + np.exp((sj * tk - hj) / (R * tk)))
-        rm = 0.5 * rd
-
-        # mesophyll CO2 partial pressure, ubar, one may use the same value as Ci assuming infinite mesohpyle conductance
-        def co2_mesophyll(ca, a_net, stomata):
-            cm = ca - a_net * stomata.total_resistance_co2() * p
-            return np.clip(cm, 0., 2*ca)
-
-        def c4(a_net, co2, rh, tleaf):
-            self.stomata.update_stomata(pressure, co2, a_net, rh, tleaf)
-            cm = co2_mesophyll(ca, a_net, self.stomata)
-
-            # PEP carboxylation rate, that is the rate of C4 acid generation
-            vp1 = (cm * vpmax) / (cm + kp)
-            vp2 = vpr
-            vp = max(min(vp1, vp2), 0)
-
-            # Enzyme limited A (Rubisco or PEP carboxylation
-            ac1 = vp + gbs*cm - rm
-            #ac1 = max(0, ac1) # prevent Ac1 from being negative Yang 9/26/06
-            ac2 = vcmax - rd
-            ac = min(ac1, ac2)
-
-            # Light and electron transport limited A mediated by J
-            #j = QuadSolnLower(theta, -(i2+jmax), i2*jmax) # rate of electron transport
-            j = min(np.roots([theta, -(i2+jmax), i2*jmax])) # rate of electron transport
-            aj1 = x*j/2. - rm + gbs*cm
-            aj2 = (1 - x)*j/3. - rd
-            aj = min(aj1, aj2)
-
-            return ((ac+aj) - ((ac+aj)**2 - 4*beta*ac*aj)**0.5) / (2*beta) # smooting the transition between Ac and Aj
-
-        def cost(x):
-            a_net0 = x[0]
-            a_net1 = c4(a_net0, self.co2, self.rh, self.tleaf)
-            return (a_net0 - a_net1)**2
-
-        #FIXME avoid passing self.stomata object to optimizer
-        # iteration to obtain Cm from Ci and A, could be re-written using more efficient method like newton-raphson method
-        res = scipy.optimize.minimize(cost, [self.a_net], options={'disp': True})
-        self.a_net = res.x[0]
-
-        self.stomata.update_stomata(pressure, self.co2, self.a_net, self.rh, self.tleaf)
-        cm = co2_mesophyll(ca, self.a_net, self.stomata)
-
-        os = alpha * self.a_net / (0.047*gbs) + om # Bundle sheath O2 partial pressure, mbar
-        #cbs = cm + (vp - a_net -rm) / gbs # Bundle sheath CO2 partial pressure, ubar
-        gamma_star = gamma1 * os
-        gamma = (rd*km + vcmax*gamma_star) / (vcmax - rd)
-        self.ci = cm
-        self.a_gross = max(0, self.a_net + rd) # gets negative when PFD = 0, Rd needs to be examined, 10/25/04, SK
 
     def _energybalance(self, jw):
         # see Campbell and Norman (1998) pp 224-225
