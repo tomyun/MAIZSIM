23d8b4e5158f00746fa3365696123bb805e2b2f5
Kyungdahm Yun <tomyun@gmail.com>
2015-09-17 11:03:51 -0700

    Implement Julian day conversion

--- driver/maizsim/io/input/base.py
+++ driver/maizsim/io/input/base.py
@@ -2,7 +2,10 @@ import datetime
 
 def date(v):
     #HACK some dates are quoted
-    return int(datetime.datetime.strptime(v.replace("'", ''), '%m/%d/%Y').strftime('%j'))
+    t = int(datetime.datetime.strptime(v.replace("'", ''), '%m/%d/%Y')
+    # convert from epoch time to Julian day
+    j = t.timestamp() / (24 * 60 * 60) + 2440587.5
+    return round(j)
 
 class LegacyFile:
     def __init__(self, filename):
