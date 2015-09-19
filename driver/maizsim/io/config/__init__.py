49fe6b22b0fcb9268e25a3d28ccae1558d0fe0e3
Kyungdahm Yun <tomyun@gmail.com>
2015-09-19 04:43:32 -0700

    Add Time submodule

--- driver/maizsim/io/config/__init__.py
+++ driver/maizsim/io/config/__init__.py
@@ -1,6 +1,7 @@
-#__all__ = ['base', 'initials', 'run', 'variety']
+#__all__ = ['base', 'initials', 'run', 'time', 'variety']
 
 from .base import date, LegacyFile
 from .initials import Initials
 from .run import Run
+from .time import Time
 from .variety import Variety
