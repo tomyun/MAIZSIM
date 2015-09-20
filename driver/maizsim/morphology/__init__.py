4dcb9f58b2721c6fb42c2b2bed23568452bc4ff6
Kyungdahm Yun <tomyun@gmail.com>
2015-09-20 13:40:13 -0700

    Add Radiation to __init__.py

--- driver/maizsim/morphology/__init__.py
+++ driver/maizsim/morphology/__init__.py
@@ -1,9 +1,10 @@
-#__all__ = ['ear', 'leaf', 'nodalunit', 'organ', 'root', 'stem', 'weight']
+#__all__ = ['ear', 'leaf', 'nodalunit', 'organ', 'radiatoin', 'root', 'stem', 'weight']
 
 from .ear import Ear
 from .leaf import Leaf
 from .nodalunit import NodalUnit
 from .organ import Organ
+from .radiation import Radiation
 from .root import Root
 from .stem import Stem
 from .weight import Weight
