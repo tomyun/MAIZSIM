c58688cfbd78d20fbb90a188a30332f09e7c816a
Kyungdahm Yun <tomyun@gmail.com>
2015-09-21 16:00:01 -0700

    Fix typo with R_max_LIR

--- driver/maizsim/phenology/stage/leafinitiation.py
+++ driver/maizsim/phenology/stage/leafinitiation.py
@@ -3,7 +3,7 @@ from ..tracker import BetaFunc
 
 class LeafInitiation(Stage):
     #FIXME use correct args
-    def setup(self, initial_leaves=5, R_max_LIR=0.0978):
+    def setup(self, initial_leaves=5, R_max_LIR=0.978):
         self.initial_leaves = initial_leaves
         self.leaves = initial_leaves
         self.R_max = R_max_LIR
