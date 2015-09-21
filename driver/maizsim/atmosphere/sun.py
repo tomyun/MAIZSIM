f6d07354fa2a1084b36d7c0a000dd191a77db68b
Kyungdahm Yun <tomyun@gmail.com>
2015-09-21 02:29:15 -0700

    Add comment on zenith_angle

--- driver/maizsim/atmosphere/sun.py
+++ driver/maizsim/atmosphere/sun.py
@@ -157,6 +157,7 @@ class Sun:
 
     @property
     def zenith_angle(self):
+        #FIXME need abs()?
         return abs(90 - self.elevation_angle)
 
     # The solar azimuth angle is the angular distance between due South and the
