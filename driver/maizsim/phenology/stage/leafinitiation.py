1e02260c24faaf888149a173b2ef08273e4bbf11
Kyungdahm Yun <tomyun@gmail.com>
2015-10-03 01:10:15 -0700

    Make leaves property
    - Fix duplicate counting of initial leaves

--- driver/maizsim/phenology/stage/leafinitiation.py
+++ driver/maizsim/phenology/stage/leafinitiation.py
@@ -5,19 +5,17 @@ class LeafInitiation(Stage):
     #FIXME use correct args
     def setup(self, initial_leaves=5, R_max_LIR=0.978):
         self.initial_leaves = initial_leaves
-        self.leaves = initial_leaves
         self.R_max = R_max_LIR
 
     def tracker(self):
-        t = BetaFunc(R_max=self.R_max)
-        t.set_initial_value(self.initial_leaves)
-        return t
-
-    def post_update(self):
-        self.leaves = self.initial_leaves + int(self.rate)
+        return BetaFunc(R_max=self.R_max)
 
     def ready(self):
         return self.pheno.germination.over()
 
     def over(self):
         return self.pheno.tassel_initiation.over()
+
+    @property
+    def leaves(self):
+        return self.initial_leaves + int(self.rate)
