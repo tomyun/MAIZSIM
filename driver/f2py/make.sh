14118d1829daf1f6ba2f81613caf3e1b52f6c93c
Kyungdahm Yun <tomyun@gmail.com>
2017-11-22 23:33:15 -0800

    Update binding builds for Python 3

--- driver/f2py/make.sh
+++ driver/f2py/make.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-find ../soil/ | grep -e ".*\.\(for\|FOR\)" | sed "/2DMAIZSIM.for/ d" | sed "/solupt.for/ d" | xargs f2py3.5 -c -m soil --f77flags="--std=legacy -fno-align-commons"
+find ../soil/ | grep -e ".*\.\(for\|FOR\)" | sed "/2DMAIZSIM.for/ d" | sed "/solupt.for/ d" | xargs f2py -c -m soil --f77flags="--std=legacy -fno-align-commons"
 ln -sf f2py/soil.so ../
--- driver/swig/make.sh
+++ driver/swig/make.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-swig -python -c++ -o crop.cc crop.i
-python setup.py build_ext --inplace
+swig -python -py3 -c++ -o crop.cc crop.i
+python3 setup.py build_ext --inplace
 ln -sf swig/_crop.so ../
 ln -sf swig/crop.py ../
